5148e58b9208607292f60ed43b8b0d480408fdb7
Soumeya HENICHE <henich_s@etna-alternance.net>
2021-11-09 14:19:13 +0100

    Add abstract class Model

---
+++ src/main/java/media/models/Model.java
@@ -0,0 +1,37 @@
+package media.models;
+
+import java.util.Objects;
+
+public abstract class Model {
+	protected Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Model [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Model other = (Model) obj;
+		return Objects.equals(id, other.id);
+	}
+}
