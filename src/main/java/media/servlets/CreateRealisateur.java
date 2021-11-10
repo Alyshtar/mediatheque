6dd89883343b3ed272f1f97635cd7f680b5e7bcd
Soumeya HENICHE <henich_s@etna-alternance.net>
2021-11-10 16:46:58 +0100

    Fix create realisateur

--- src/main/java/media/servlets/CreateRealisateur.java
+++ src/main/java/media/servlets/CreateRealisateur.java
@@ -1,6 +1,5 @@
 package media.servlets;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 
+import media.dao.DaoException;
 import media.dao.DaoFactory;
 import media.dao.ModelDao;
 import media.models.Realisateur;
@@ -24,73 +23,66 @@ import media.servlets.processing.DataProcessing;
 public class CreateRealisateur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ModelDao realisateurDao;
-	
-       
-    
-    public CreateRealisateur() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+
+
+	public CreateRealisateur() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	public void init() throws ServletException{
 		this.realisateurDao=DaoFactory.getInstance().getDao("realisateur");
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8");
-		
+
 		int status=200;
 		String content=null;
 		String erreur="";
-		
+
 		try {
-			StringBuffer buffer = new StringBuffer();
-			String line = null;
-			BufferedReader reader = request.getReader();
-			while ((line = reader.readLine()) != null) {
-			buffer.append(line);
-			}
-		
-		JsonObject data = JsonParser.parseString(buffer.toString()).getAsJsonObject();
-		
-		
-		String nom=data.get("nom").getAsString();
-		String prenom=data.get("prenom").getAsString();
-		Integer age=data.get("age").getAsInt();
-		String pays=data.get("pays").getAsString();
-		
-		if(nom!=null) {
-			if(nom.length()>100) {
-				erreur="le nom du realisateur doit contenir  moins de 100 caracteres.";
-			}
-		}else {erreur="le nom du realisateur est obligatoire";}
-		
-		if(prenom!=null) {
-			if(prenom.length()<2) {
-				erreur="le prenom du realisateur doit contenir moins de 100 caracteres.";
-			}
-		}else {erreur="le prenom de l'auteur est obligatoire";}
-		
-		if(age==null) {
-			
+
+			JsonObject data = DataProcessing.getJsonFromRequest(request);
+
+
+			String nom=data.get("nom").getAsString();
+			String prenom=data.get("prenom").getAsString();
+			Integer age=data.get("age").getAsInt();
+			String pays=data.get("pays").getAsString();
+
+			if(nom!=null) {
+				if(nom.length()>100) {
+					erreur="le nom du realisateur doit contenir  moins de 100 caracteres.";
+				}
+			}else {erreur="le nom du realisateur est obligatoire";}
+
+			if(prenom!=null) {
+				if(prenom.length()<2) {
+					erreur="le prenom du realisateur doit contenir moins de 100 caracteres.";
+				}
+			}else {erreur="le prenom de l'auteur est obligatoire";}
+
+			if(age==null) {
+
 				erreur="l'age du realisateur est obligatoire";
-		}
-		
-		if(pays.length()>200) {
-			erreur="le nom du pays d'origine du realisateur doit contenir  moins de 200 caracteres.";
-		}
-		
-		if(erreur.equals("")) {
-			Realisateur realisateur= new Realisateur(nom, prenom, age, pays);
-			realisateurDao.create(realisateur);
-			response.getWriter().write("ok");}
-		
-		else {
-			response.setStatus(400);
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().write("Erreur : "+erreur);
 			}
-		}catch (media.dao.DaoException e) {
-			// TODO: handle exception
+
+			if(pays.length()>200) {
+				erreur="le nom du pays d'origine du realisateur doit contenir  moins de 200 caracteres.";
+			}
+
+			if(erreur.equals("")) {
+				Realisateur realisateur= new Realisateur(nom, prenom, age, pays);
+				realisateurDao.create(realisateur);
+				response.getWriter().write("ok");}
+
+			else {
+				response.setStatus(400);
+				response.setCharacterEncoding("UTF-8");
+				response.getWriter().write("Erreur : "+erreur);
+			}
+		}catch (DaoException e) {
 			e.printStackTrace();
 			response.setStatus(500);
 			response.getWriter().write("erreur DAO");
@@ -108,8 +100,8 @@ public class CreateRealisateur extends HttpServlet {
 			}
 			response.setStatus(status);
 			response.getWriter().write(content);
-		
-	}
 
-}
+		}
+
+	}
 }
