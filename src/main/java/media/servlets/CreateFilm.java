04aba5bed8a4312946b7d500b33d7f7049a2c365
Soumeya HENICHE <henich_s@etna-alternance.net>
2021-11-10 14:32:51 +0100

    Update Film CRUD
    Added basic try catch in appropriate function
    Added ModelDao attribute in servlets

--- src/main/java/media/servlets/CreateFilm.java
+++ src/main/java/media/servlets/CreateFilm.java
@@ -7,12 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonSyntaxException;
+
+import media.dao.DaoException;
+import media.dao.DaoFactory;
+import media.dao.ModelDao;
+
 /**
  * Servlet implementation class CreateFilm
  */
 @WebServlet(urlPatterns={"/film/create", "film/new"})
 public class CreateFilm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ModelDao filmDao;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -21,21 +28,43 @@ public class CreateFilm extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    
+    public void init() throws ServletException{
+    	this.filmDao = DaoFactory.getInstance().getDao("film");
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setCharacterEncoding("UTF-8");
+
+		int status = 200;
+		String content = null;
+		String erreur = "";
+		
+		try {
+			
+			
+		} catch (JsonSyntaxException e) {
+			e.printStackTrace();
+			erreur += "Wrong JSON format";
+			status = 400;
+		} catch (DaoException e) {
+			e.printStackTrace();
+			erreur += " 500 - "+e.getMessage();
+			status = 500;
+		} catch (Exception e) {
+			e.printStackTrace();
+			erreur += "Unknown server error\n";
+			status = 400;
+		} finally {
+			if(!erreur.equals("")) {
+				content = "Erreur : \n"+erreur;
+			}
+			response.setStatus(status);
+			response.getWriter().write(content);
+		}
 	}
 
 }
--- src/main/java/media/servlets/DeleteFilm.java
+++ src/main/java/media/servlets/DeleteFilm.java
@@ -7,12 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonSyntaxException;
+
+import media.dao.DaoException;
+import media.dao.DaoFactory;
+import media.dao.ModelDao;
+
 /**
  * Servlet implementation class DeleteFilm
  */
 @WebServlet("/film/delete")
 public class DeleteFilm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ModelDao filmDao;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -21,21 +28,44 @@ public class DeleteFilm extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    public void init() throws ServletException{
+    	this.filmDao = DaoFactory.getInstance().getDao("film");
+    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doDelete(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+
+		int status = 200;
+		String content = null;
+		String erreur = "";
+		
+		try {
+			
+			
+		} catch (JsonSyntaxException e) {
+			e.printStackTrace();
+			erreur += "Wrong JSON format";
+			status = 400;
+		} catch (DaoException e) {
+			e.printStackTrace();
+			erreur += " 500 - "+e.getMessage();
+			status = 500;
+		} catch (Exception e) {
+			e.printStackTrace();
+			erreur += "Unknown server error\n";
+			status = 400;
+		} finally {
+			if(!erreur.equals("")) {
+				content = "Erreur : \n"+erreur;
+			}
+			response.setStatus(status);
+			response.getWriter().write(content);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
